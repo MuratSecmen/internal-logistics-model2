0850d17e28d5e284952f15f73567ad4fb7d99747
Murat Seçmen <93551765+MuratSecmen@users.noreply.github.com>
2026-05-15 07:03:30 +0300

    Delete codes/from pathlib import Path.py

--- codes/from pathlib import Path.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-INPUT_DIR = Path(r"C:\Users\Asus\Documents\GitHub\internal-logistics-model2\internal-logistics-model2\inputs")
-
-print("Klasör var mı?", INPUT_DIR.exists())
-print("Klasördeki dosyalar:")
-
-for f in INPUT_DIR.iterdir():
-    print(f.name)
